13ea5b693976cf7d7f7915131f4047c387d757fa
anthony collier <anthony-collier@hotmail.fr>
2021-12-04 00:39:50 +0100

    Store is now persisted after page reloading

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vuex from 'vuex';
 import tasks from './modules/tasks';
 import users from './modules/users';
+import createPersistedState from "vuex-persistedstate";
 
 export default new Vuex.Store({
     modules:{
         tasks,
         users
-    }
+    },
+    plugins: [createPersistedState()]
 });
