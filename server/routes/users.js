b3750fa36b4d0bb6862b9fb4ba3dfe002c1d0ae6
anthony collier <anthony-collier@hotmail.fr>
2021-12-03 23:55:06 +0100

    added bcrypt

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
+const jwt = require('jsonwebtoken');
 
 //Get all users
 router.get('/', async (req,res) => {
